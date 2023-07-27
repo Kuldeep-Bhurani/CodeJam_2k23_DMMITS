1c826889fd60d267668adecfe51aeb988f845ad9
Kuldeep Bhurani <84840495+Kuldeep-Bhurani@users.noreply.github.com>
2023-07-27 07:25:53 -0700

    heroVid Id added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let mouseCursor = document.querySelector(".cursor");
 let cards = document.querySelectorAll(".card");
 let cursorDiv = document.getElementById("cursor");
+let vid = document.getElementById("heroVid");
+
+vid.playbackRate = 0.5;
 
 window.addEventListener('mousemove', cursor);
 
